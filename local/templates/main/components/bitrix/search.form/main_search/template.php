1dd8f91e3b7dddfe1b8be5a87ae6307b6e7b7252
Your Name <you@example.com>
2023-09-06 15:32:17 +0300

    Change search html

--- local/templates/main/components/bitrix/search.form/main_search/template.php
+++ local/templates/main/components/bitrix/search.form/main_search/template.php
@@ -11,25 +11,35 @@
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);?>
-<div class="search-form">
-<form action="<?=$arResult["FORM_ACTION"]?>">
-	<table border="0" cellspacing="0" cellpadding="2" align="center">
-		<tr>
-			<td align="center"><?if($arParams["USE_SUGGEST"] === "Y"):?><?$APPLICATION->IncludeComponent(
-				"bitrix:search.suggest.input",
-				"",
-				array(
-					"NAME" => "q",
-					"VALUE" => "",
-					"INPUT_SIZE" => 15,
-					"DROPDOWN_SIZE" => 10,
-				),
-				$component, array("HIDE_ICONS" => "Y")
-			);?><?else:?><input type="text" name="q" value="" size="15" maxlength="50" /><?endif;?></td>
-		</tr>
-		<tr>
-			<td align="right"><input name="s" type="submit" value="<?=GetMessage("BSF_T_SEARCH_BUTTON");?>" /></td>
-		</tr>
-	</table>
-</form>
+
+<div class="gray-bg">
+    <div class="container">
+        <form class="search clearfix" action="<?=$arResult["FORM_ACTION"]?>">
+            <input class="search__txt" name="q" value="" placeholder="Поиск по названию, артикулу, EAN " type="search">
+            <button class="search__submit" name="s" type="submit"><span>Найти</span></button>
+        </form>
+    </div>
 </div>
+
+<!--<div class="search-form">-->
+<!--<form action="--><?php //=$arResult["FORM_ACTION"]?><!--">-->
+<!--	<table border="0" cellspacing="0" cellpadding="2" align="center">-->
+<!--		<tr>-->
+<!--			<td align="center">--><?//if($arParams["USE_SUGGEST"] === "Y"):?><!----><?//$APPLICATION->IncludeComponent(
+//				"bitrix:search.suggest.input",
+//				"",
+//				array(
+//					"NAME" => "q",
+//					"VALUE" => "",
+//					"INPUT_SIZE" => 15,
+//					"DROPDOWN_SIZE" => 10,
+//				),
+//				$component, array("HIDE_ICONS" => "Y")
+//			);?><!----><?//else:?><!--<input type="text" name="q" value="" size="15" maxlength="50" />--><?//endif;?><!--</td>-->
+<!--		</tr>-->
+<!--		<tr>-->
+<!--			<td align="right"><input name="s" type="submit" value="--><?php //=GetMessage("BSF_T_SEARCH_BUTTON");?><!--" /></td>-->
+<!--		</tr>-->
+<!--	</table>-->
+<!--</form>-->
+<!--</div>-->
