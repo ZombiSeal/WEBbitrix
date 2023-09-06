33bf852671b02433af736bb5b343c927b7cde48d
Your Name <you@example.com>
2023-09-06 16:41:17 +0300

    Changed main_search_page template

--- local/templates/main/components/bitrix/search.page/main_search_page/template.php
+++ local/templates/main/components/bitrix/search.page/main_search_page/template.php
@@ -21,82 +21,25 @@
 		$obSearchSuggest->SetResultCount($arResult["NAV_RESULT"]->NavRecordCount);
 	}
 	?>
-	<?$APPLICATION->IncludeComponent(
-		"bitrix:search.suggest.input",
-		"",
-		array(
-			"NAME" => "q",
-			"VALUE" => $arResult["REQUEST"]["~QUERY"],
-			"INPUT_SIZE" => 40,
-			"DROPDOWN_SIZE" => 10,
-			"FILTER_MD5" => $arResult["FILTER_MD5"],
-		),
-		$component, array("HIDE_ICONS" => "Y")
-	);?>
+<!--	--><?//$APPLICATION->IncludeComponent(
+//		"bitrix:search.suggest.input",
+//		"",
+//		array(
+//			"NAME" => "q",
+//			"VALUE" => $arResult["REQUEST"]["~QUERY"],
+//			"INPUT_SIZE" => 40,
+//			"DROPDOWN_SIZE" => 10,
+//			"FILTER_MD5" => $arResult["FILTER_MD5"],
+//		),
+//		$component, array("HIDE_ICONS" => "Y")
+//	);?>
 <?else:?>
 	<input type="text" name="q" value="<?=$arResult["REQUEST"]["QUERY"]?>" size="40" />
 <?endif;?>
-<?if($arParams["SHOW_WHERE"]):?>
-	&nbsp;<select name="where">
-	<option value=""><?=GetMessage("SEARCH_ALL")?></option>
-	<?foreach($arResult["DROPDOWN"] as $key=>$value):?>
-	<option value="<?=$key?>"<?if($arResult["REQUEST"]["WHERE"]==$key) echo " selected"?>><?=$value?></option>
-	<?endforeach?>
-	</select>
-<?endif;?>
+
 	&nbsp;<input type="submit" value="<?=GetMessage("SEARCH_GO")?>" />
-	<input type="hidden" name="how" value="<?echo $arResult["REQUEST"]["HOW"]=="d"? "d": "r"?>" />
-<?if($arParams["SHOW_WHEN"]):?>
-	<script>
-	var switch_search_params = function()
-	{
-		var sp = document.getElementById('search_params');
-		var flag;
-		var i;
-
-		if(sp.style.display == 'none')
-		{
-			flag = false;
-			sp.style.display = 'block'
-		}
-		else
-		{
-			flag = true;
-			sp.style.display = 'none';
-		}
-
-		var from = document.getElementsByName('from');
-		for(i = 0; i < from.length; i++)
-			if(from[i].type.toLowerCase() == 'text')
-				from[i].disabled = flag;
-
-		var to = document.getElementsByName('to');
-		for(i = 0; i < to.length; i++)
-			if(to[i].type.toLowerCase() == 'text')
-				to[i].disabled = flag;
-
-		return false;
-	}
-	</script>
-	<br /><a class="search-page-params" href="#" onclick="return switch_search_params()"><?echo GetMessage('CT_BSP_ADDITIONAL_PARAMS')?></a>
-	<div id="search_params" class="search-page-params" style="display:<?echo $arResult["REQUEST"]["FROM"] || $arResult["REQUEST"]["TO"]? 'block': 'none'?>">
-		<?$APPLICATION->IncludeComponent(
-			'bitrix:main.calendar',
-			'',
-			array(
-				'SHOW_INPUT' => 'Y',
-				'INPUT_NAME' => 'from',
-				'INPUT_VALUE' => $arResult["REQUEST"]["~FROM"],
-				'INPUT_NAME_FINISH' => 'to',
-				'INPUT_VALUE_FINISH' =>$arResult["REQUEST"]["~TO"],
-				'INPUT_ADDITIONAL_ATTR' => 'size="10"',
-			),
-			null,
-			array('HIDE_ICONS' => 'Y')
-		);?>
-	</div>
-<?endif?>
-</form><br />
+<!--	<input type="hidden" name="how" value="--><?//echo $arResult["REQUEST"]["HOW"]=="d"? "d": "r"?><!--" />-->
+</form>
 
 <?if(isset($arResult["REQUEST"]["ORIGINAL_QUERY"])):
 	?>
