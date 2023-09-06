e4d30a73cbd389fce677ad4453d0a57b35a4f162
Your Name <you@example.com>
2023-09-06 15:14:41 +0300

    Add index.php file

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Какой-то заголовок");
+//$USER->Authorize(1);
+?>
+    <p>HELLO!!!!!</p>
+</div>
+</div>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
