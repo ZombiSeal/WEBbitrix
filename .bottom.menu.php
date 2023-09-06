1ae1975fa66b3afe8f34789badc318d7dd30a6df
Your Name <you@example.com>
2023-09-06 15:15:41 +0300

    Add menu files

---
+++ .bottom.menu.php
@@ -0,0 +1,46 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Каталог товаров", 
+		"catalog/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"О компании", 
+		"/test.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Партнерам", 
+		"/test.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Торговые марки", 
+		"/test.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Статьи", 
+		"/test.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Контакты", 
+		"/test.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
---
+++ .submenu.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Новая страница", 
+		"/novaya-stranitsa.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Бытовые аккумуляторы", 
+		"/bytovye-akkumulyatory/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
